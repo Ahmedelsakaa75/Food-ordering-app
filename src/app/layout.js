329aeaa1a06764d3cdac64a0d86bd69a4dce0270
Ahmed Elsakaa <ahmedelsakaa75@gmail.com>
2024-10-22 13:57:34 +0300

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,11 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import {Roboto} from 'next/font/google'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight:['400', '500', '700']
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -21,10 +16,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.className} antialiased`}
       >
         {children}
       </body>
     </html>
   );
 }
+ 
