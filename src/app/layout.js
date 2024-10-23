53ee1ed08d0fa08c49cf2ef5d516798b9bf44a18
Ahmed Elsakaa <ahmedelsakaa75@gmail.com>
2024-10-23 11:03:07 +0300

    Create Headeer And Hero Components

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${roboto.className} antialiased`}
       >
+        <main className="max-w-4xl mx-auto p-4">
         {children}
+        </main>
       </body>
     </html>
   );
---
+++ src/components/layout/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from 'next/image';
+import pizzaImage from '../../../public/pizza.png'
+import RightArrow from "../icons/right";
+
+export default function Hero() {
+  return (
+    <section className="hero"> 
+     <div className="py-12">
+     <h1 className=" text-4xl font-semibold">Everything <br /> is better <br />with a <span className="text-primary">Pizza</span></h1>
+      <p className="my-4 text-gray-500 text-sm">
+        Pizza is the missing piece that makes every day complete, a simple yet
+        delicious joy in life.
+      </p>
+      <div className="flex gap-4 text-sm">
+        <button className="flex items-center gap-2 bg-primary text-white uppercase px-4 py-2 rounded-full">Order Now <RightArrow className="size-6"/></button>
+        <button className="flex items-center gap-2 py-2 text-gray-600 font-semibold">Learn More <RightArrow /></button>
+      </div>
+     </div>
+      <div className="relative">
+      <Image layout={'fill'} objectFit={'contain'} src={pizzaImage} alt={"Pizza"} /> 
+      </div>
+    </section>
+  );
+}
